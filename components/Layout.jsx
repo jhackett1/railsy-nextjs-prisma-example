eef34b5aa55b3725b3ddd24e8d414c71d520654d
Jaye Hackett <jaye.hackett@gmail.com>
2021-03-26 23:44:49 +0000

    update layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,34 +5,25 @@ import { signOut, useSession } from "next-auth/client"
 const Layout = props => {
   const [session, loading] = useSession()
 
-  let links
-
-  if (loading) {
-    links = <p>Validating session...</p>
-  }
-
-  if (!session) {
-    links = (
-      <p>
-        <Link href="/api/auth/signin">
-          <a>Log in</a>
-        </Link>
-      </p>
-    )
-  }
-
   if (session) {
-    links = (
-      <p>
-        You are logged in as {session.user.email || "Unknown email"}
-        <button onClick={() => signOut()}>Log out</button>
-      </p>
+    return (
+      <>
+        <p>
+          You are logged in as {session.user.email || "Unknown email"}
+          <button onClick={() => signOut()}>Log out</button>
+        </p>
+        {props.children}
+      </>
     )
   }
 
   return (
     <>
-      {links}
+      <p>
+        <Link href="/api/auth/signin">
+          <a>Log in</a>
+        </Link>
+      </p>
       {props.children}
     </>
   )
